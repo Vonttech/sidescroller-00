eec09d32cd28030dad0c60c850a6d0c43af03e2a
Victor Matheus <victormatheus183@gmail.com>
2022-06-17 16:53:31 -0300

    Pendulum trap script created

---
+++ Sidescroller-00/Assets/Scripts/SpikedBall.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikedBall : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    [SerializeField] float leftPushRange;
+    [SerializeField] float rightPushRange;
+    [SerializeField] float velocityThreshold;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.angularVelocity = velocityThreshold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Push();
+    }
+    private void Push()
+    {
+        if (transform.rotation.z > 0
+        && transform.rotation.z < rightPushRange
+        && (rigidbody2D.angularVelocity > 0)
+        && rigidbody2D.angularVelocity < velocityThreshold)
+        {
+            rigidbody2D.angularVelocity = velocityThreshold;
+        }
+        else if (transform.rotation.z < 0
+       && transform.rotation.z > rightPushRange
+       && (rigidbody2D.angularVelocity < 0)
+       && rigidbody2D.angularVelocity > velocityThreshold * -1)
+        {
+            rigidbody2D.angularVelocity = velocityThreshold * -1;
+        }
+
+    }
+}
