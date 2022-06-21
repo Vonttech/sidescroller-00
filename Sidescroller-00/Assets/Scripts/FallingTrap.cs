d05a7bc464102822a2cef1011937f958e3d1c8a7
Victor Matheus <victormatheus183@gmail.com>
2022-06-21 20:20:27 -0300

    start and update deleted

--- Sidescroller-00/Assets/Scripts/FallingTrap.cs
+++ Sidescroller-00/Assets/Scripts/FallingTrap.cs
@@ -7,18 +7,6 @@ public class FallingTrap : MonoBehaviour
     [SerializeField]
     private float timeToFall = 2f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player")){
