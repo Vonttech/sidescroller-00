6c87a136d6b529fd8c6a66f910c95ee266790575
Victor Matheus <victormatheus183@gmail.com>
2022-06-23 00:44:42 -0300

    Enemy hardcode removed

--- Sidescroller-00/Assets/Scripts/Enemy.cs
+++ Sidescroller-00/Assets/Scripts/Enemy.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject target;
+    private Transform target;
+
+    [SerializeField]
+    private float maxDistanceAllowed = 2.75f;
+    [SerializeField]
+    private float moveSpeed = 2.75f;
+
+    private float distance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, target.transform.position, 0.2f * Time.deltaTime);
+        Move();
+    }
+   private void Move()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+
+        if(distance < maxDistanceAllowed)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+        }
     }
 }
