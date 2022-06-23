94976d2862d1471bb5e65183241ba2dfc849fd17
Victor Matheus <victormatheus183@gmail.com>
2022-06-23 00:46:46 -0300

    trap toggler class disabled

--- Sidescroller-00/Assets/Scripts/TrapToggler.cs
+++ Sidescroller-00/Assets/Scripts/TrapToggler.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class TrapToggler : MonoBehaviour
 {
-    [SerializeField]
-    private float activeTrap = 6;
+    //[SerializeField]
+    //private float activeTrap = 6;
 
-    private bool isActive = false;
+    //private bool isActive = false;
 
-    private GameObject player;
+    //private GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    player = GameObject.FindGameObjectWithTag("Player");
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(Vector3.Distance(player.transform.position, transform.position));
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //Debug.Log(Vector3.Distance(player.transform.position, transform.position));
 
-        if (Vector3.Distance(transform.position, player.transform.position) < activeTrap)
-        {
-            isActive = true; 
-            Debug.DrawLine(transform.position, player.transform.position, Color.red);
-            Debug.Log(Vector3.Distance(player.transform.position, transform.position));
-            //transform.position += ((player.transform.position - transform.position) * Time.deltaTime * 0.55f);
+    //    if (Vector3.Distance(transform.position, player.transform.position) < activeTrap)
+    //    {
+    //        isActive = true; 
+    //        //Debug.DrawLine(transform.position, player.transform.position, Color.red);
+    //        //Debug.Log(Vector3.Distance(player.transform.position, transform.position));
+    //        //transform.position += ((player.transform.position - transform.position) * Time.deltaTime * 0.55f);
 
-            transform.position += new Vector3(0, 0.10f, 0) * Time.deltaTime * 0.2f;
-        }
-        else
-        {
-            isActive = false;
-        }
-    }
+    //        transform.position += new Vector3(0, 0.10f, 0) * Time.deltaTime * 0.2f;
+    //    }
+    //    else
+    //    {
+    //        isActive = false;
+    //    }
+    //}
 }
