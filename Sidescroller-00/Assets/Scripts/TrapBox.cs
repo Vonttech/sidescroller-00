107ffd1928226a37e1f49c27c941985b68a3c683
Victor Matheus <victormatheus183@gmail.com>
2022-06-23 21:55:45 -0300

    trapbox implemented
    the trapbox contains a trap that will appear after the box destruction;
    
    the destruction will occur when the target distance is near of the trapbox

---
+++ Sidescroller-00/Assets/Scripts/TrapBox.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBox : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject innerTrap;
+
+    private Transform targetTransform;
+
+    private Animator animator;
+
+    [SerializeField]
+    private float maxDistanceAllowed;
+
+    private float targetDistance;
+
+    private bool disableBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTargetDistance();
+    }
+
+    private void CheckTargetDistance()
+    {
+        targetDistance = Vector3.Distance(targetTransform.position, transform.position);
+        
+        if(targetDistance <= maxDistanceAllowed)
+        {
+            animator.SetTrigger("destroy");
+
+            StartCoroutine(DisableBox());
+
+        }
+    }
+
+
+    IEnumerator DisableBox()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        gameObject.SetActive(false);
+
+        innerTrap.transform.position = transform.position;
+
+        innerTrap.SetActive(true);
+    }
+}
