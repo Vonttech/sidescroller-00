a488c08abbadb3380b28b88c5b3b1928f310e717
Victor Matheus <victormatheus183@gmail.com>
2022-06-26 19:25:23 -0300

    Checkpoint feature
    Checkpoint prefab created;
    
    animation created;
    
    animation trigger implemented for when the player hits the checkpoint collider will displays the animation

---
+++ Sidescroller-00/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Animator animator;
+
+    public static bool checkpointActivated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetTrigger("playerHitCheckpoint");
+            animator.SetBool("checkpointActivated", true);
+            checkpointActivated = true;
+        }
+    }
+}
