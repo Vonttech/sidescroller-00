a5cd9ace3e0f0bb29c470272fe971b5e9c39488f
Victor Matheus <36866913+Vonttech@users.noreply.github.com>
2022-07-02 17:54:02 -0300

    LifePoints UI system implemented

--- Sidescroller-00/Assets/Scripts/GameManager.cs
+++ Sidescroller-00/Assets/Scripts/GameManager.cs
@@ -19,13 +19,27 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI pointsTextField;
 
+    [SerializeField]
+    private RawImage[] lifePointsImageGameObject = new RawImage[3];
+
+    [SerializeField]
+    private Sprite loseLifePointImage;
+
+    private int playerLifePointsCount;
+
     public static int fruitPoints = 0;
+
+    private void Start()
+    {
+        playerLifePointsCount = playerScript.LifePoints;
+    }
 
     // Update is called once per frame
     void Update()
     {
         RestartFromCheckpoint();
         CountPoints();
+        CheckPlayerLifePoints();
     }
 
     private void RestartFromCheckpoint()
@@ -41,5 +55,17 @@ public class GameManager : MonoBehaviour
     private void CountPoints()
     {
         pointsTextField.text = fruitPoints.ToString();
+    }
+
+    private void CheckPlayerLifePoints()
+    {
+        if(playerLifePointsCount > playerScript.LifePoints)
+        {
+            int lastLifePointIndex = playerLifePointsCount - 1;
+
+            lifePointsImageGameObject[lastLifePointIndex].texture = loseLifePointImage.texture;
+
+            playerLifePointsCount--;
+        }
     }
 }
