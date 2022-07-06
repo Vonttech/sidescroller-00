38479322e4ac4e3ff11834f4f6e1e16031e84c60
Victor Matheus <36866913+Vonttech@users.noreply.github.com>
2022-07-05 22:27:48 -0300

    rank logic

--- Sidescroller-00/Assets/Scripts/Player/PlayerData.cs
+++ Sidescroller-00/Assets/Scripts/Player/PlayerData.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public static class PlayerData
 {
     public static int playerFruitPoints;
@@ -8,45 +10,46 @@ public static class PlayerData
 
     public static string PlayerLevelRank()
     {
-        //float playerFruitPointsMean = playerFruitPoints / LevelData.totalFruitsInLevel;
+        float playerFruitPointsMean = playerFruitPoints * 1.75f / LevelData.totalFruitsInLevel;
 
-        //float playerLifePointsMean = playerLifePoints / playerInitialLifePoints;
+        float playerLifePointsMean = playerLifePoints * 0.30f / playerInitialLifePoints;
 
-        //float playerRankMean = (playerFruitPointsMean + playerLifePointsMean + 0.25f) / 2;
+        float playerRankMean = (playerFruitPointsMean + playerLifePointsMean ) / 2;
 
+        if (playerRankMean >= 1f)
+        {
+            return "S";
+        }
+        else if (playerRankMean < 1f &&
+                 playerRankMean >= 0.8f)
+        {
+            return "A";
+        }
+        else if (playerRankMean <= 0.8f &&
+                 playerRankMean >= 0.6f)
+        {
+            return "B";
+        }
+        else if (playerRankMean <= 0.6f &&
+                 playerRankMean >= 0.4f)
+        {
+            return "C";
+        }
+        else if (playerRankMean <= 0.4f &&
+                 playerRankMean >= 0.2f)
+        {
+            return "D";
+        }
+        if (playerRankMean <= 0.2f &&
+                 playerRankMean >= 0.1f)
+        {
+            return "E";
+        }
+        else
+        {
+            return "F";
+        }
 
-        //if (playerRankMean == 1f)
-        //{
-        //    return "S";
-        //}else if(playerRankMean < 1f &&
-        //         playerRankMean > 0.8f)
-        //{
-        //    return "A";
-        //}
-        //else if(playerRankMean < 0.8f &&
-        //         playerRankMean > 0.6f)
-        //{
-        //    return "B";
-        //}else if(playerRankMean < 0.6f &&
-        //         playerRankMean > 0.4f)
-        //{
-        //    return "C";
-        //}
-        //else if(playerRankMean < 0.4f &&
-        //         playerRankMean > 0.2f)
-        //{
-        //    return "D";
-        //}
-        //if (playerRankMean < 0.2f &&
-        //         playerRankMean > 0.1f)
-        //{
-        //    return "E";
-        //}
-        //else
-        //{
-        //    return "F";
-        //}
-        return "teste";
     }
 
 }
