a1e23d549450d352998db1cd5bb8deab90f83939
Victor Matheus <36866913+Vonttech@users.noreply.github.com>
2022-07-05 22:00:42 -0300

    LevelData variables
    Level data now stores spawn points locations

--- Sidescroller-00/Assets/Scripts/LevelData.cs
+++ Sidescroller-00/Assets/Scripts/LevelData.cs
@@ -1,7 +1,12 @@
-
+using UnityEngine;
 public static class LevelData
 {
 
     public static int totalFruitsInLevel;
+
+    public static Vector3 checkpointPosition;
+
+    public static Vector3 levelStartPoint;
    
+
 }
