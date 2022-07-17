f67ed29fd0799d37c3484aa9b529cb24f54bb938
Victor Matheus <victormatheus183@gmail.com>
2022-07-17 16:56:02 -0300

    checkpoint audioclip set
    checkpoint now triggers a audio when is actived

--- Sidescroller-00/Assets/Scripts/Checkpoint.cs
+++ Sidescroller-00/Assets/Scripts/Checkpoint.cs
@@ -9,8 +9,10 @@ public class Checkpoint : MonoBehaviour
     public static bool isPlayerTouchedCheckpoint = false;
     public static int timesCheckpointUsed = 0;
     public static bool isLastRespawnAllowed = false;
-    
+
     private Animator animator;
+    private AudioSource audioSource;
+    private Collider2D collider;
     private int checkpointUseLimit = 4;
     public int CheckpointUseLimit { get { return checkpointUseLimit; } }
 
@@ -18,16 +20,25 @@ public class Checkpoint : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        collider = GetComponent<Collider2D>();
+        
          if (isCheckpointActivated)
         {
             animator.SetTrigger("playerHitCheckpoint");
             animator.SetBool("checkpointActivated", isCheckpointActivated);
+        }
+        else
+        {
+            collider.enabled = true;
         }    
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !isCheckpointActivated)
         {
+            collider.enabled = false;
+            audioSource.Play();
             isPlayerTouchedCheckpoint = true;
             isCheckpointActivated = true;
             PlayerData.playerLifePointsSaved = PlayerData.playerLifePoints;
