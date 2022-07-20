f9b6af0c82535fdd61585bef5f240b0a55aba8ae
Victor Matheus <victormatheus183@gmail.com>
2022-07-20 11:31:02 -0300

    empty lines deleted

--- Sidescroller-00/Assets/Scripts/Item.cs
+++ Sidescroller-00/Assets/Scripts/Item.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     private Animator animator;
-
     private Collider2D itemCollider;
-
     private float timeToDisable = 0.3f;
-
     private AudioSource itemAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-
         if (Player.itemsCollected.ContainsKey(gameObject.name))
         {
             gameObject.SetActive(false);
@@ -23,31 +19,9 @@ public class Item : MonoBehaviour
         else
         {
             gameObject.SetActive(true);
-
             animator = GetComponent<Animator>();
-
             itemCollider = GetComponent<Collider2D>();
-
             itemAudioSource = GetComponent<AudioSource>();
-        }
-    }
-
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            itemAudioSource.Play();
-
-            itemCollider.enabled = false;
-
-            animator.SetBool("collected", true);
-
-            Player.itemsCollected.Add(gameObject.name, gameObject);
-
-            PlayerData.playerFruitPoints++;
-
-            StartCoroutine(DisableItem());
         }
     }
 
@@ -56,4 +30,19 @@ public class Item : MonoBehaviour
         yield return new WaitForSeconds(timeToDisable);
         gameObject.SetActive(false);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            itemAudioSource.Play();
+            itemCollider.enabled = false;
+            animator.SetBool("collected", true);
+            Player.itemsCollected.Add(gameObject.name, gameObject);
+            PlayerData.playerFruitPoints++;
+            StartCoroutine(DisableItem());
+        }
+    }
+
+   
 }
